04f49440e332fa496e4355a66538c6d4c94307b6
RiccardoAD <rick_95@libero.it>
2024-05-15 23:52:49 +0200

    .

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
 
 include __DIR__ . '/db.php';
-include __DIR__ . '/header.php';
+
 
 ?>
+<!doctype html>
+<html lang="en">
 
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>progetto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    
+    <div class="container">
 
 <h1>Login</h1>
 <form method="post" action="">
